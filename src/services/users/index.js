e05ff0fb1ef7a7f4e81b48f2e865aecd71702a7f
Necati Erol <necati.erol@wext.com>
2021-09-13 03:54:10 +0300

    user service added

---
+++ src/services/users/index.js
@@ -0,0 +1,47 @@
+import Models from '../../models';
+
+import validators from './validators';
+
+
+const settings = {
+  online: {
+    method: 'post',
+    path: '/users',
+    validation: validators.online
+  },
+  updateScore: {
+    method: 'put',
+    path: '/users/:id/score'
+  }
+};
+
+const defaultQueries = {};
+
+class UserService {
+  online = async (req) => {
+    const { body } = req;
+
+    let user = await Models.User.findOne({ userId: Number(body.userId) }).exec();
+    if (!user) {
+      try {
+        user = await Models.User.create(body);
+      } catch (error) {
+        return {
+          status: 500,
+          body: { error }
+        };
+      }
+    }
+
+    return {
+      status: 200,
+      body: user.toJSON()
+    };
+  };
+}
+
+export default {
+  handlers: new UserService(),
+  settings,
+  defaultQueries
+};
