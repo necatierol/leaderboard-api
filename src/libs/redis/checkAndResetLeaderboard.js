1f6ca2403ff6fa511261566be40da549267e94b1
Necati Erol <necati.erol@wext.com>
2021-09-14 07:02:56 +0300

    fix

--- src/libs/redis/checkAndResetLeaderboard.js
+++ src/libs/redis/checkAndResetLeaderboard.js
@@ -4,10 +4,10 @@ import CacheLib from '.';
 import { USER_COUNT } from '../../constants/leaderboard';
 
 
-export default async (userId, rank = USER_COUNT + 1) => {
+export default async (userId, rank) => {
   let userCache = await CacheLib.findUserInLeaderboard(userId);;
 
-  if (rank <= USER_COUNT && (!userCache || userCache.rank !== rank)) {
+  if (rank !== 0 && rank <= USER_COUNT && (!userCache || userCache.rank !== rank)) {
     CacheLib.setLeaderboard(await DataLib.getLeaderboard());
   }
 };
