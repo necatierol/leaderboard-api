f6512fc4a9c6359cf7ea6dd30df570b444d54680
Necati Erol <necati.erol@wext.com>
2021-09-14 06:50:53 +0300

    score request has been limited

--- src/services/users/index.js
+++ src/services/users/index.js
@@ -5,6 +5,7 @@ import calculateScore from '../../libs/score/calculate';
 import getUserRank from '../../libs/mongo/getUserRank';
 
 import validators from './validators';
+import { PERIOD } from '../../constants/score';
 
 
 const settings = {
@@ -47,13 +48,25 @@ class UserService {
 
   updateScore = async (req) => {
     const { userScore, prizePoolScore } = calculateScore();
-    const userId = Number(req.params.id);
 
-    let user;
+    const user = await Models.User.findOne({ userId: Number(req.params.id) });
+
+    const updatedTime = new Date(user.updatedAt).getTime();
+    const now = new Date().getTime();
+
+    if (Math.abs(now - updatedTime) < PERIOD) {
+      return {
+        status: 429,
+        body: { error: { details: ['Too Many Requests'] }}
+      };
+    }
+
     await Promise.all([
       (async () => {
-        user = await Models.User
-          .findOneAndUpdate({ userId }, { $inc: { score: userScore } });
+        if (user.lastRank === 0) user.lastRank = await getUserRank(user.score);
+        user.score += userScore;
+
+        await user.save();
       })(),
       (async () => {
         await Models.PrizePool
@@ -61,13 +74,8 @@ class UserService {
       })()
     ]);
 
-
-    const userRank = await getUserRank(user.score);
-    CacheLib.checkAndResetLeaderboard(user.userId, userRank);
-
-    if (user.lastRank === 0) {
-      await Models.User.updateOne({ userId }, { lastRank: userRank });
-    }
+    const userNewRank = await getUserRank(user.score);
+    CacheLib.checkAndResetLeaderboard(user.userId, userNewRank);
 
     return {
       status: 200,
